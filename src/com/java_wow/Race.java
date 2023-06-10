eb350ec34ca0677eab292ebfed9d9e26699b8488
João Marcelo Dantas <jmgrd98@gmail.com>
2023-06-10 20:36:36 -0300

    race class

--- src/com/java_wow/Race.java
+++ src/com/java_wow/Race.java
@@ -4,10 +4,26 @@ public class Race {
 
     private String name;
     private String description;
-    private int strength;
-    private int dexterity;
-    private int constitution;
-    private int intelligence;
-    private int wisdom;
-    private int charisma;
+
+    public Race(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        System.out.println("Name: " + this.name);
+        return this.name;
+    }
+    public void setName(String name) {
+        this.name = name;
+        System.out.println("Name: " + this.name);
+    }
+    public String getDescription() {
+        System.out.println("Description: " + this.description);
+        return this.description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+        System.out.println("Description: " + this.description);
+    }
 }
